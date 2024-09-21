ee8f6717046b457b55af2ac64596bed00eb78b11
Myo Aung <myo@Myos-MacBook-Air.local>
2024-09-20 19:41:48 -0700

    test3

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+    int yes {3};
+    int b {yes + 1}; //both work = prob easier
+    int po = 4;
+
+ std::cout << b ;
+ std::cout << "yo";
+
+ return 0;
+
+}
+
